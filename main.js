1f47319d0f22c5d4a65a93d3f3e3a920adc92ab3
Michelangelo Ruisi <mruisi96@gmail.com>
2024-12-21 11:28:22 +0100

    modificati else e default
    adesso nel caso in cui l'utente inserisca un input che non sia un numero riceverà il messaggio che il dato deve essere un numero che indichi gradi o temperatura

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let temperatura = prompt("Inserisci qui la temperatura percepita:");
+let temperatura = Number(prompt("Inserisci qui la temperatura percepita:"));
 
 if (temperatura >= 0 && temperatura <= 20) {
     console.log("Non ci sono più le mezze stagioni");
@@ -8,13 +8,15 @@ if (temperatura >= 0 && temperatura <= 20) {
     console.log("Lu mare, lu sole, lu ientu");
 }else if (temperatura < 0 && temperatura >= -10) {
     console.log("non è tanto il freddo quanto l’umidità");
-}else {
+}else if (temperatura < -10){
     console.log("Copriti... ancora ti raffreddi");
+}else {
+    console.log("Inserisci un numero che indichi la temperatura");
 }
 
 // creati intervalli corretti per evitare che il <30 assorbisse tutte le altre condizioni, dalla traccia non si evince tale necessità.
 
-let gradi = prompt("Inserisci qui i gradi percepiti:");
+let gradi = Number(prompt("Inserisci qui i gradi percepiti:"));
 
 switch (true) {
     case gradi >= 0 && gradi <= 20 : console.log("Non ci sono più le mezze stagioni");
@@ -25,7 +27,9 @@ switch (true) {
         break;
     case gradi < 0 && gradi >= -10 : console.log("non è tanto il freddo quanto l’umidità");
         break;
-    default : console.log("Copriti... ancora ti raffreddi");
+    case temperatura < -10 : console.log("Copriti... ancora ti raffreddi");
+        break;
+    default : console.log("Inserisci un numero che indichi i gradi");
 }
 
 // lasciati entrambi i prompt che si susseguono per testare in contemporanea i 2 esercizi.
